7681e02ef5cf33b2e82fb872ce28ebfa5183695b
artem_shalaev00@mail.ru <XBHPOTFSWJDF937>
2023-01-13 20:12:51 +0300

    ver 2.1

---
+++ src/Family.java
@@ -0,0 +1,8 @@
+public class Family {
+    public static void main(String[] args) {
+        System.out.println("Oleg");
+        System.out.println("Vladimir");
+        System.out.println("Natasha");
+        System.out.println("Artem");
+    }
+}
