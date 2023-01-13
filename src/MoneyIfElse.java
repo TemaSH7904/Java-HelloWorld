4a70e1892cd0bff7c1ad1ff043094840a4440dd3
artem_shalaev00@mail.ru <XBHPOTFSWJDF937>
2023-01-13 21:40:51 +0300

    ver 2.3

---
+++ src/MoneyIfElse.java
@@ -0,0 +1,12 @@
+public class MoneyIfElse {
+    public static void main(String[] args) {
+        int money = 12;
+        if (money > 10) {
+            System.out.println("Пицца");
+        } else if (money > 6){
+            System.out.println("Гамбургер");
+        } else {
+            System.out.println("Сэндвич");
+        }
+    }
+}
