655305e04346b8cf520e99249b5b87ab0b1efefc
artem_shalaev00@mail.ru <XBHPOTFSWJDF937>
2023-01-16 22:32:55 +0300

    ver FloatDouble

---
+++ src/FloatDouble.java
@@ -0,0 +1,10 @@
+public class FloatDouble {
+    public static void main(String[] args) {
+        int dad = 50;
+        int mom = 48;
+        int bro = 17;
+        int sum = dad + mom + bro;
+        double avg = (double) sum/3;
+        System.out.println(avg);
+    }
+}
