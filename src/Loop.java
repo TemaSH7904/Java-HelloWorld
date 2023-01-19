8c39a144e384ebf16b0e0eb8ba6976f501bd0746
artem_shalaev00@mail.ru <XBHPOTFSWJDF937>
2023-01-20 01:38:08 +0300

    ver Loop

--- src/Loop.java
+++ src/Loop.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 
 public class Loop {
     public static void main(String[] args) {
-//        int number = 0;
+    int number = 1000;
 //        while (number <= 1000) {
 //            if (number % 2 == 0)
 //            System.out.println(number);
 //            number++;
 //      }
-
+        for (int i = 1000; i >= 0; i--) {
+            if (i % 3 == 0){
+                System.out.println(i);
+            }
+        }
     }
 }
